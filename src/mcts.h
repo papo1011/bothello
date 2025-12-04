dbbc484de1973d6900b7e0cdee8c65930441d423
papo1011 <francesco.papini.siena@gmail.com>
2025-12-04 12:57:41 +0100

    feat(mcts): add MCTS class to header file

--- src/mcts.h
+++ src/mcts.h
@@ -2,13 +2,18 @@
 
 #include "board.h"
 #include <memory>
+#include <random>
 #include <vector>
 
-// MCTS Node structure
+// Monte-Carlo Tree Search Node structure
 struct Node {
     Board state;
-    Node* parent;
-    uint64_t move_from_parent; // The move that created this node (0 indicates a PASS)
+
+    // Pointer to the parent node is used for backpropagation
+    Node *parent;
+
+    // The move that created this node (0 indicates a PASS)
+    uint64_t move_from_parent;
 
     // Using unique_ptr for automatic memory management (RAII)
     std::vector<std::unique_ptr<Node>> children;
@@ -18,19 +23,45 @@ struct Node {
 
     double wins;
     int visits;
-    int player_moved_to_create_node; // 0 for Root's player, 1 for Opponent
 
-    Node(const Board& state, Node* parent = nullptr, uint64_t move = 0);
+    // 0 for Root's player, 1 for Opponent
+    int player_moved_to_create_node;
+
+    Node(Board const &state, Node *parent = nullptr, uint64_t move = 0);
 
     bool is_fully_expanded() const;
     bool is_terminal() const;
 
-    // Selects the best child using the UCB1 formula
-    Node* best_child(double c_param = 1.414) const;
+    // Selects the best child using the UCB formula
+    Node *best_child(double c_param = 1.414) const;
 
     // Expands the tree by adding a new child node
-    Node* expand();
+    Node *expand();
 
     // Updates the stats for this node
     void update(double result);
+};
+
+class MCTS {
+  public:
+    MCTS(int iterations);
+
+    Move get_best_move(Board const &state);
+
+  private:
+    int iterations;
+
+    /*  Marsenne Twister random number generator
+    Used instead of rand() because avoids modulo bias */
+    std::mt19937 rng;
+
+    /*  Selection phase: traverse the tree to a leaf node using UCB
+    Expansion phase: create new child node and return pointer to it */
+    Node *tree_policy(Node *node);
+
+    // Simulation phase: play out a random game from the given state
+    double default_policy(Board state);
+
+    // Backpropagation phase: update the nodes with the simulation result
+    void backpropagate(Node *node, double result);
 };
